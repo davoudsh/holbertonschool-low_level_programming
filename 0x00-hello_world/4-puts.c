2117854661f255e3a3494e9b768eb69385361330
davoudsh <davoud_shafiee@yahoo.com>
2018-01-18 10:44:08 +0000

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
+ * Description - Printing lines
  */
 int main(void)
 {
-	  printf("with proper grammar, but the outcome is a piece of art,\n");
-  	  return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
