49cf8ff28a2a7f7284cdc6d47647f012b34a6664
davoudsh <davoud_shafiee@yahoo.com>
2018-01-18 10:12:21 +0000

    holberton

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * prints "with proper grammar, but the outcome is a piece of art,
  * ends with 0
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
+	 return (0);
 }
+
