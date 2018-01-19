d3424f8bdf29e252d8d6d8735b7fa06bc32f4ab1
davoudsh <davoud_shafiee@yahoo.com>
2018-01-19 08:37:06 +0000

    h

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 /**
- * main - print base 10 numbers
+ * main - prints alphabet in reverse
  * Return: 0 if successful
  */
 
 int main(void)
 {
 char c;
-for (c = 'z'; c > 'z'; c--)
-{
+for (c = 'z'; c >= 'a'; c--)
 putchar(c);
-}
 putchar('\n');
 return (0);
 }
