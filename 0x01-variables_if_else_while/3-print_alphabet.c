845913f4ae3e8cff9dea0a7791f3ecc0c5798fb5
davoudsh <davoud_shafiee@yahoo.com>
2018-01-19 07:50:33 +0000

    holberton'

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**                                                                                                                                                                                    * main - for loop that prints alphabet                                                                                                                                                 *                                                                                                                                                                                      * Return: Always 0 (Success)                                                                                                                                                           */
+int main(void)
+{
+char c = 'a';
+char C = 'A';
+while (c <= 'z')
+{
+putchar (c);
+c++;
+}
+while (C <= 'Z')
+{ 
+putchar (C);
+C++;
+}
+putchar ('\n');
+return (0);
+}
