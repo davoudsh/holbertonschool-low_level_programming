cb81b3b4773d58e1a339084fcc735a21b635b1b5
davoudsh <davoud_shafiee@yahoo.com>
2018-01-19 08:35:07 +0000

    h

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print base 10 numbers
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+char c;
+for (c = 'z'; c > 'z'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
