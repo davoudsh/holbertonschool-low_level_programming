9481a038e02d45d2090841a89c34a7bed81fdfcf
davoudsh <davoud_shafiee@yahoo.com>
2018-01-19 08:05:02 +0000

    holberton

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print base 10 numbers
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+int n = 0;
+
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+printf("\n");
+return (0);
+}
